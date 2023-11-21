0ece29b7209483c0075dcc76ee624e73b98362b5
gcastrot82 <gcastrot82@hotmail.com>
2023-11-20 21:09:55 -0500

    Update: Se agrega pruebas automatizadas

---
+++ cypress/e2e/crearproyectos-spec.cy.js
@@ -0,0 +1,30 @@
+require('cypress-xpath');
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://loadbalancerproyectoabc-735612126.us-east-2.elb.amazonaws.com:4200')
+    cy.wait(1000)
+
+    cy.xpath('/html/body/app-root/div/app-login/div/div[1]/form/div[1]/div[1]/div/input').should('be.visible').type("empresa");
+    cy.wait(1000)
+
+    cy.xpath('/html/body/app-root/div/app-login/div/div[1]/form/div[2]/div/div/input').should('be.visible').type("empresa");
+    cy.wait(1000)
+
+    cy.xpath('/html/body/app-root/div/app-login/div/div[1]/form/div[3]/button[1]').click();
+    cy.wait(1000)
+
+    cy.xpath('/html/body/app-root/div/app-banner/div/div/h1').should('exist');
+    cy.xpath('/html/body/app-root/div/app-banner/div/div/h1').invoke('text').then((text) => {
+      // Verificar que el texto contiene el nombre de la empresa esperado
+      const nombreDeLaEmpresaLogueada = 'empresa';
+      expect(text.toLowerCase()).to.include(nombreDeLaEmpresaLogueada.toLowerCase());
+    });
+    cy.wait(1000)
+
+    cy.xpath('//*[@id="navbarNavDropdown"]/ul/li[3]').click();
+    cy.wait(1000)
+      
+   
+
+  })
+})
